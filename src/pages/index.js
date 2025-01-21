9a49dbe176170f95a5145d3b04672000c3cfb789
KielLagmay <auventurous.buddy@gmail.com>
2025-01-21 17:51:24 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,47 +9,49 @@ export default function Home() {
   return(
     <Layout menuBarItems={[(<MenuWindow pageID="home" />)]} showMenuBar={true}>
     <SEO title="Home" description="Welcome to the Au-venturous Buddy Official Website!" />
-    <section className="py-3 m-3">
-      <section className="py-3 hero">
-        <div style={{textAlign: `center`, margin: 0}}>
+    <div className="explore-page">
+      <section className="py-3 m-3 explore-page-body">
+        <section className="py-3 hero">
+          <div style={{textAlign: `center`, margin: 0}}>
+            <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
+            What's New?
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 860,
+                padding: `1.45rem 1.0875rem`
+              }}
+              className="mb-4"
+            >
+              <div style={{ margin: 0 }}>
+                <StaticImage src="../images/AUVBxNACW.png" alt="National Autism Consciousness Week" layout="fullWidth"></StaticImage>
+              </div>
+            </div>
+            </ResponsiveHeader>
+          </div>
+        </section>
+        <section>
           <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
-          What's New?
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 860,
-              padding: `1.45rem 1.0875rem`
-            }}
-            className="mb-4"
-          >
-            <div style={{ margin: 0 }}>
-              <StaticImage src="../images/AUVBxNACW.png" alt="National Autism Consciousness Week" layout="fullWidth"></StaticImage>
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 860,
+                padding: `1.45rem 1.0875rem`,
+                borderRadius: `50%`,
+                border: `#00F`
+              }}
+              className="mb-4"
+            >
+              <div style={{ margin: 0 }}>
+                <StaticImage src="../images/KaliProfile.png" alt="Kali's Profile Picture" layout="fullWidth"></StaticImage>
+              </div>
             </div>
-          </div>
+            Inspiring AU-some Buddy, AU-some Dad, AU-some Mom to have an AU-some AU-venture!
           </ResponsiveHeader>
-        </div>
+          Welcome to Au-venturous Buddy, a place where Au-venturous buddies can explore and learn new things from fellow Au-venturous buddies!
+        </section>
       </section>
-      <section>
-        <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 860,
-              padding: `1.45rem 1.0875rem`,
-              borderRadius: `50%`,
-              border: `#00F`
-            }}
-            className="mb-4"
-          >
-            <div style={{ margin: 0 }}>
-              <StaticImage src="../images/KaliProfile.png" alt="Kali's Profile Picture" layout="fullWidth"></StaticImage>
-            </div>
-          </div>
-          Inspiring AU-some Buddy, AU-some Dad, AU-some Mom to have an AU-some AU-venture!
-        </ResponsiveHeader>
-        Welcome to Au-venturous Buddy, a place where Au-venturous buddies can explore and learn new things from fellow Au-venturous buddies!
-      </section>
-    </section>
+    </div>
     </Layout>
   )
 }
