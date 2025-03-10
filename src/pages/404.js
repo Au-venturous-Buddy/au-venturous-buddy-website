3614289d665a0a1509027cbbe6046bfd31ae6c3a
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-03-10 14:50:23 +0800

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,29 +8,29 @@ import MenuWindow from "../components/menu-window";
 export default function FourZeroFour() {
   return(
     <Layout menuBarItems={[(<MenuWindow pageID={null} />)]} showMenuBar={true}>
-      <SEO title="404" description="Page Not Found!" />
-      <section style={{marginBottom: `90px`}}>
-        <div style={{textAlign: `center`}}>
-        <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 860,
-              padding: `1.45rem 1.0875rem`,
-            }}
-          >
-            <div style={{ margin: 0 }}>
-              <StaticImage src="../images/Zene N Zeanne V4 FantaZZticFive.png" alt="The FantaZZtic Five" layout="fullWidth"></StaticImage>
+    <SEO title="404" description="Page Not Found!" />
+    <div className="explore-page">
+      <section className="py-3 m-3 explore-page-body">
+        <section className="py-3 hero">
+          <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
+            <div
+              style={{
+                margin: `0 auto`,
+                maxWidth: 460,
+                padding: `1.45rem 1.0875rem`
+              }}
+              className="mb-4"
+            >
+              <div style={{ margin: 0 }} className="kali-profile-image">
+                <StaticImage src="../images/KaliProfile.png" alt="Kali's Profile Picture" layout="fullWidth"></StaticImage>
+              </div>
             </div>
-          </div>
-          Looks like this page doesn't exist yet!
+            Looks like this page doesn't exist yet!
           </ResponsiveHeader>
-        </div>
-        <br />
-        <p style={{textAlign: `center`}}>
           Please navigate to an existing page via the main menu below.
-        </p>
+        </section>
       </section>
+    </div>
     </Layout>
   )
 }
