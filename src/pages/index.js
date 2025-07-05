aea41384e16752e3102d99c3b41c0a22c6c149ff
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-07-05 21:06:28 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               className="mb-4"
             >
               <div style={{ margin: 0 }} className="hero-image">
-                <StaticImage src="../images/June2025.png" alt="June 2025" layout="fullWidth"></StaticImage>
+                <StaticImage src="../images/July2025.png" alt="July 2025" layout="fullWidth"></StaticImage>
               </div>
             </div>
             </ResponsiveHeader>
