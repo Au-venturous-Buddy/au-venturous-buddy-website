b8b3f90b93d6b126651a77c41df734741bde01ab
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-10-21 11:33:34 +0800

    Add GetQuestionnaires hook to fetch questionnaire data

---
+++ src/hooks/get-questionnaires.js
@@ -0,0 +1,33 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+export const GetQuestionnaires = () => {
+    const questionnaireData = useStaticQuery(
+        graphql`
+        query {
+          allFile(
+            filter: {relativeDirectory: {regex: "/assets/ASDScreen.*/"}, ext: {eq: ".md"}, name: {eq: "index"}}
+            sort: {relativePath: DESC}
+          ) {
+            edges {
+              node {
+                relativeDirectory
+                name
+                ext
+                childMarkdownRemark {
+                  frontmatter {
+                    title
+                  }
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        }
+        `
+        )
+    
+    return questionnaireData
+}
+      
