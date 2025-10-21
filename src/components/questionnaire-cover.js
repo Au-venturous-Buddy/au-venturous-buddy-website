595b07fa971670b01f43cbaf369a488fd7dd94d4
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-10-21 11:34:21 +0800

    Add QuestionnaireCover component for questionnaire preview

---
+++ src/components/questionnaire-cover.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Button } from "react-bootstrap"
+import ResponsiveHeader from "./responsive-header";
+import {BsPlayCircle} from "react-icons/bs";
+
+export default function QuestionnaireCover({title, slug}) {
+  return(
+    <Button className="my-3" href={slug}>
+      <div className="questionnaire-preview">
+        <div className="questionnaire-preview-title">
+          <ResponsiveHeader level={2} maxSize={2} minScreenSize={460}>
+            {title} <br />
+            <BsPlayCircle aria-hidden />
+          </ResponsiveHeader>
+        </div>
+      </div>
+    </Button>
+  )
+}
