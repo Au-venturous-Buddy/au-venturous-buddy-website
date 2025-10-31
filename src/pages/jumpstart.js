5a1b16cc102595cbeadff3f20ded2ebdefbd2dbe
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-10-31 12:12:54 +0800

    Increase iframe height for video and form displays

--- src/pages/jumpstart.js
+++ src/pages/jumpstart.js
@@ -32,14 +32,14 @@ export default function HomePage() {
         <section>
             <div className="m-3 tablet-display-blue">
               <div className="tablet-display-body">
-                <iframe width="100%" height="420" src="https://www.youtube.com/embed/UdgvdcIeaiA"></iframe>
+                <iframe width="100%" height="840" src="https://www.youtube.com/embed/UdgvdcIeaiA"></iframe>
               </div>
             </div>
         </section>
         <section>
             <div className="m-3 tablet-display-pink">
               <div className="tablet-display-body">
-                <iframe width="100%" height="420" src="https://docs.google.com/forms/d/e/1FAIpQLSelDYy3ULEXE6bP18MmknDVjyUQ5tHnoYlp-YKebUIQtwwIvA/viewform?embedded=true"></iframe>
+                <iframe width="100%" height="840" src="https://docs.google.com/forms/d/e/1FAIpQLSelDYy3ULEXE6bP18MmknDVjyUQ5tHnoYlp-YKebUIQtwwIvA/viewform?embedded=true"></iframe>
               </div>
             </div>
         </section>
