bf47e4e4dcdfa63acd35522ce56ed421b32b0ac1
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-10-31 10:14:57 +0800

    Create jumpstart.js for Jumpstart Program page

---
+++ src/pages/jumpstart.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo";
+import ResponsiveSize from "../hooks/responsive-size";
+import MenuWindow from "../components/menu-window";
+import ResponsiveHeader from "../components/responsive-header";
+import { StaticImage } from "gatsby-plugin-image"
+
+export default function HomePage() {
+  return(
+    <Layout menuBarItems={[(<MenuWindow pageID="jumpstart" />)]} showMenuBar={true}>
+      <SEO title="AUVBxBBP Jumpstart Program Page" description="AUVBxBBP Jumpstart Program Page" />
+      <section className="pt-3 mt-3 mb-5" style={{textAlign: "center", paddingBottom: "1in"}}>
+        <section className="mx-3 hero-section">
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 260,
+              padding: `1.45rem 1.0875rem`,
+              textAlign: `center`
+            }}
+          >
+            <div style={{ margin: 0 }}>
+              <ResponsiveHeader level={1} maxSize={2} minScreenSize={800}>
+                <StaticImage src="../images/October2025.jpg" alt='AUVBxBBP Jumpstart Logo' layout="fullWidth"></StaticImage>
+              </ResponsiveHeader>
+            </div>
+          </div>
+        </section>
+      </section>
+    </Layout>
+  )
+}
