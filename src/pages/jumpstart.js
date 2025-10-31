e139556d74f42188a9414c00ea219cf78d937bd3
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-10-31 13:33:19 +0800

    Wrap iframes in divs for better layout control

--- src/pages/jumpstart.js
+++ src/pages/jumpstart.js
@@ -32,14 +32,18 @@ export default function HomePage() {
         <section>
             <div className="m-3 tablet-display-blue">
               <div className="tablet-display-body">
-                <iframe width="100%" height="840" src="https://www.youtube.com/embed/UdgvdcIeaiA"></iframe>
+                <div className="tablet-display-screen">
+                  <iframe width="100%" height="840" src="https://www.youtube.com/embed/UdgvdcIeaiA"></iframe>
+                </div>
               </div>
             </div>
         </section>
         <section>
             <div className="m-3 tablet-display-pink">
               <div className="tablet-display-body">
-                <iframe width="100%" height="840" src="https://docs.google.com/forms/d/e/1FAIpQLSelDYy3ULEXE6bP18MmknDVjyUQ5tHnoYlp-YKebUIQtwwIvA/viewform?embedded=true"></iframe>
+                <div className="tablet-display-screen">
+                  <iframe width="100%" height="840" src="https://docs.google.com/forms/d/e/1FAIpQLSelDYy3ULEXE6bP18MmknDVjyUQ5tHnoYlp-YKebUIQtwwIvA/viewform?embedded=true"></iframe>
+                </div>
               </div>
             </div>
         </section>
