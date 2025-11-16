8894c43ef3a6de8db4d36d0adcaaf7e02b60a065
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-11-16 18:10:20 +0800

    Replace October 2025 image with November 2025 image

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               className="mb-4"
             >
               <div style={{ margin: 0 }} className="hero-image">
-                <StaticImage src="../images/October2025.png" alt="October 2025" layout="fullWidth"></StaticImage>
+                <StaticImage src="../images/November2025.png" alt="November 2025" layout="fullWidth"></StaticImage>
               </div>
             </div>
             </ResponsiveHeader>
