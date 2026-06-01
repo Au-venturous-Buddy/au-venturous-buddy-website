43671cc8538e386d37fd8b0ff2dfd1584286ba0f
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2026-06-01 09:29:52 +0800

    Replace April 2026 image with May 2026 image

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
               className="mb-4"
             >
               <div style={{ margin: 0 }} className="hero-image">
-                <StaticImage src="../images/April2026.png" alt="April 2026" layout="fullWidth"></StaticImage>
+                <StaticImage src="../images/May2026.png" alt="May 2026" layout="fullWidth"></StaticImage>
               </div>
             </div>
             </ResponsiveHeader>
